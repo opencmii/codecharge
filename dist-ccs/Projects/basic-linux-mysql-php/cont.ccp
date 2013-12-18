1c3efc526365416098b3ca9dc5ca5deac1923cd6
opencmii <cmii@opentimi.org>
2013-12-18 07:19:22 -0500

    a

--- dist-ccs/Projects/basic-linux-mysql-php/cont.ccp
+++ dist-ccs/Projects/basic-linux-mysql-php/cont.ccp
@@ -1,4 +1,4 @@
-<Page id="1" templateExtension="html" relativePath="." fullRelativePath="." secured="False" urlType="Relative" isIncluded="False" SSLAccess="False" isService="False" cachingEnabled="False" cachingDuration="1 minutes" needGeneration="0" useDesign="False" masterPage="{CCS_PathToMasterPage}/MasterPage.ccp">
+<Page id="1" templateExtension="html" relativePath="." fullRelativePath="." secured="False" urlType="Relative" isIncluded="False" SSLAccess="False" isService="False" cachingEnabled="False" cachingDuration="1 minutes" needGeneration="0" useDesign="False" masterPage="Designs/theme-wet-boew/MasterPage.ccp">
 	<Components>
 		<Panel id="13" visible="True" generateDiv="False" name="Head" contentPlaceholder="Head">
 			<Components/>
@@ -6,31 +6,7 @@
 			<Attributes/>
 			<Features/>
 		</Panel>
-		<Panel id="14" visible="True" generateDiv="False" name="TopMenu" contentPlaceholder="TopMenu">
-			<Components>
-				<IncludePage id="11" name="gcwu_gcnb" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="TopMenugcwu_gcnb" page="inc/topMenu.ccp">
-					<Components/>
-					<Events/>
-					<Features/>
-				</IncludePage>
-			</Components>
-			<Events/>
-			<Attributes/>
-			<Features/>
-		</Panel>
-		<Panel id="15" visible="True" generateDiv="False" name="CustomCSS" contentPlaceholder="CustomCSS">
-			<Components/>
-			<Events/>
-			<Attributes/>
-			<Features/>
-		</Panel>
-		<Panel id="16" visible="True" generateDiv="False" name="CustomScripts" contentPlaceholder="CustomScripts">
-			<Components/>
-			<Events/>
-			<Attributes/>
-			<Features/>
-		</Panel>
-		<Panel id="17" visible="True" generateDiv="False" name="Footer" contentPlaceholder="Footer">
+<Panel id="17" visible="True" generateDiv="False" name="Footer" contentPlaceholder="Footer">
 			<Components>
 				<IncludePage id="8" name="gcwu_footer" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Footergcwu_footer" page="inc/footer.ccp">
 					<Components/>
@@ -42,31 +18,19 @@
 			<Attributes/>
 			<Features/>
 		</Panel>
-		<Panel id="20" visible="True" generateDiv="False" name="Content" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Content" contentPlaceholder="Content">
-			<Components>
-				<IncludePage id="24" name="main_basic" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Contentmain_basic" page="main.ccp">
-					<Components/>
-					<Events/>
-					<Features/>
-				</IncludePage>
-			</Components>
-			<Events/>
-			<Attributes/>
-			<Features/>
-		</Panel>
-		<Panel id="21" visible="True" generateDiv="False" name="SiteMenu" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="SiteMenu" contentPlaceholder="SiteMenu">
-			<Components>
-				<IncludePage id="22" name="gcwu_psnb" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="SiteMenugcwu_psnb" page="inc/siteMenu.ccp">
-					<Components/>
-					<Events/>
-					<Features/>
-				</IncludePage>
-			</Components>
-			<Events/>
-			<Attributes/>
-			<Features/>
-		</Panel>
-		<Panel id="18" visible="True" generateDiv="False" name="Breadcrum" contentPlaceholder="cgwu_breadcrum">
+<Panel id="27" visible="True" generateDiv="False" name="CustomScripts">
+<Components/>
+<Events/>
+<Attributes/>
+<Features/>
+</Panel>
+<Panel id="28" visible="True" generateDiv="False" name="CustomCSS">
+<Components/>
+<Events/>
+<Attributes/>
+<Features/>
+</Panel>
+<Panel id="18" visible="True" generateDiv="False" name="Breadcrum" contentPlaceholder="cgwu_breadcrum">
 			<Components>
 				<IncludePage id="19" name="gcwu_breadcrum" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Breadcrumgcwu_breadcrum" page="inc/breadcrum.ccp">
 					<Components/>
@@ -78,13 +42,43 @@
 			<Attributes/>
 			<Features/>
 		</Panel>
-		<Panel id="23" visible="True" generateDiv="False" name="NavMenu" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="NavMenu">
-			<Components/>
+<Panel id="21" visible="True" generateDiv="False" name="SiteMenu" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="SiteMenu" contentPlaceholder="SiteMenu">
+			<Components>
+				<IncludePage id="22" name="gcwu_psnb" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="SiteMenugcwu_psnb" page="inc/siteMenu.ccp">
+					<Components/>
+					<Events/>
+					<Features/>
+				</IncludePage>
+			</Components>
 			<Events/>
 			<Attributes/>
 			<Features/>
 		</Panel>
-	</Components>
+<Panel id="14" visible="True" generateDiv="False" name="TopMenu" contentPlaceholder="TopMenu" masterPage="Designs/theme-wet-boew/MasterPage.ccp">
+			<Components>
+				<IncludePage id="11" name="gcwu_gcnb" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="TopMenugcwu_gcnb" page="inc/topMenu.ccp">
+					<Components/>
+					<Events/>
+					<Features/>
+				</IncludePage>
+			</Components>
+			<Events/>
+			<Attributes/>
+			<Features/>
+		</Panel>
+<Panel id="29" visible="True" generateDiv="False" name="Content" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Content">
+<Components>
+<IncludePage id="30" name="main" wizardTheme="None" wizardThemeType="File" wizardThemeVersion="3.0" PathID="Contentmain" page="main.ccp">
+<Components/>
+<Events/>
+<Features/>
+</IncludePage>
+</Components>
+<Events/>
+<Attributes/>
+<Features/>
+</Panel>
+</Components>
 	<CodeFiles>
 		<CodeFile id="Code" language="PHPTemplates" name="cont.php" forShow="True" url="cont.php" comment="//" codePage="utf-8"/>
 	</CodeFiles>
--- dist-ccs/Projects/basic-linux-mysql-php/cont.php
+++ dist-ccs/Projects/basic-linux-mysql-php/cont.php
@@ -13,28 +13,28 @@ include_once(RelativePath . "/Sorter.php");
 include_once(RelativePath . "/Navigator.php");
 //End Include Common Files
 
-//Master Page implementation @1-04BB7BEF
-include_once(RelativePath . "/Designs/theme-gcwu-intranet/MasterPage.php");
+//Master Page implementation @1-CC89CAAB
+include_once(RelativePath . "/Designs/theme-wet-boew/MasterPage.php");
 //End Master Page implementation
-
-//Include Page implementation @11-2813BFAB
-include_once(RelativePath . "/inc/topMenu.php");
-//End Include Page implementation
 
 //Include Page implementation @8-E5D4CE8B
 include_once(RelativePath . "/inc/footer.php");
 //End Include Page implementation
 
-//Include Page implementation @24-65164D5D
-include_once(RelativePath . "/main.php");
+//Include Page implementation @19-5188A416
+include_once(RelativePath . "/inc/breadcrum.php");
 //End Include Page implementation
 
 //Include Page implementation @22-A45EDC57
 include_once(RelativePath . "/inc/siteMenu.php");
 //End Include Page implementation
 
-//Include Page implementation @19-5188A416
-include_once(RelativePath . "/inc/breadcrum.php");
+//Include Page implementation @11-2813BFAB
+include_once(RelativePath . "/inc/topMenu.php");
+//End Include Page implementation
+
+//Include Page implementation @30-65164D5D
+include_once(RelativePath . "/main.php");
 //End Include Page implementation
 
 //Initialize Page @1-9F99DBCB
@@ -70,61 +70,56 @@ $Scripts = "|";
 $CCSEventResult = CCGetEvent($CCSEvents, "BeforeInitialize", $MainPage);
 //End Before Initialize
 
-//Initialize Objects @1-3076BFE5
+//Initialize Objects @1-5DF91654
 $Attributes = new clsAttributes("page:");
 $Attributes->SetValue("pathToRoot", $PathToRoot);
 $MainPage->Attributes = & $Attributes;
 
 // Controls
-$MasterPage = new clsMasterPage("/Designs/" . $CCProjectDesign . "/", "MasterPage", $MainPage);
+$MasterPage = new clsMasterPage("/Designs/theme-wet-boew/", "MasterPage", $MainPage);
 $MasterPage->Attributes = $Attributes;
 $MasterPage->Initialize();
 $Head = new clsPanel("Head", $MainPage);
 $Head->PlaceholderName = "Head";
-$TopMenu = new clsPanel("TopMenu", $MainPage);
-$TopMenu->PlaceholderName = "TopMenu";
-$gcwu_gcnb = new clstopMenu("inc/", "gcwu_gcnb", $MainPage);
-$gcwu_gcnb->Initialize();
-$CustomCSS = new clsPanel("CustomCSS", $MainPage);
-$CustomCSS->PlaceholderName = "CustomCSS";
-$CustomScripts = new clsPanel("CustomScripts", $MainPage);
-$CustomScripts->PlaceholderName = "CustomScripts";
 $Footer = new clsPanel("Footer", $MainPage);
 $Footer->PlaceholderName = "Footer";
 $gcwu_footer = new clsfooter("inc/", "gcwu_footer", $MainPage);
 $gcwu_footer->Initialize();
-$Content = new clsPanel("Content", $MainPage);
-$Content->PlaceholderName = "Content";
-$main_basic = new clsmain("", "main_basic", $MainPage);
-$main_basic->Initialize();
-$SiteMenu = new clsPanel("SiteMenu", $MainPage);
-$SiteMenu->PlaceholderName = "SiteMenu";
-$gcwu_psnb = new clssiteMenu("inc/", "gcwu_psnb", $MainPage);
-$gcwu_psnb->Initialize();
+$CustomScripts = new clsPanel("CustomScripts", $MainPage);
+$CustomCSS = new clsPanel("CustomCSS", $MainPage);
 $Breadcrum = new clsPanel("Breadcrum", $MainPage);
 $Breadcrum->PlaceholderName = "cgwu_breadcrum";
 $gcwu_breadcrum = new clsbreadcrum("inc/", "gcwu_breadcrum", $MainPage);
 $gcwu_breadcrum->Initialize();
-$NavMenu = new clsPanel("NavMenu", $MainPage);
+$SiteMenu = new clsPanel("SiteMenu", $MainPage);
+$SiteMenu->PlaceholderName = "SiteMenu";
+$gcwu_psnb = new clssiteMenu("inc/", "gcwu_psnb", $MainPage);
+$gcwu_psnb->Initialize();
+$TopMenu = new clsPanel("TopMenu", $MainPage);
+$TopMenu->PlaceholderName = "TopMenu";
+$gcwu_gcnb = new clstopMenu("inc/", "gcwu_gcnb", $MainPage);
+$gcwu_gcnb->Initialize();
+$Content = new clsPanel("Content", $MainPage);
+$main = new clsmain("", "main", $MainPage);
+$main->Initialize();
 $MainPage->Head = & $Head;
-$MainPage->TopMenu = & $TopMenu;
-$MainPage->gcwu_gcnb = & $gcwu_gcnb;
-$MainPage->CustomCSS = & $CustomCSS;
-$MainPage->CustomScripts = & $CustomScripts;
 $MainPage->Footer = & $Footer;
 $MainPage->gcwu_footer = & $gcwu_footer;
-$MainPage->Content = & $Content;
-$MainPage->main_basic = & $main_basic;
-$MainPage->SiteMenu = & $SiteMenu;
-$MainPage->gcwu_psnb = & $gcwu_psnb;
+$MainPage->CustomScripts = & $CustomScripts;
+$MainPage->CustomCSS = & $CustomCSS;
 $MainPage->Breadcrum = & $Breadcrum;
 $MainPage->gcwu_breadcrum = & $gcwu_breadcrum;
-$MainPage->NavMenu = & $NavMenu;
-$TopMenu->AddComponent("gcwu_gcnb", $gcwu_gcnb);
+$MainPage->SiteMenu = & $SiteMenu;
+$MainPage->gcwu_psnb = & $gcwu_psnb;
+$MainPage->TopMenu = & $TopMenu;
+$MainPage->gcwu_gcnb = & $gcwu_gcnb;
+$MainPage->Content = & $Content;
+$MainPage->main = & $main;
 $Footer->AddComponent("gcwu_footer", $gcwu_footer);
-$Content->AddComponent("main_basic", $main_basic);
-$SiteMenu->AddComponent("gcwu_psnb", $gcwu_psnb);
 $Breadcrum->AddComponent("gcwu_breadcrum", $gcwu_breadcrum);
+$SiteMenu->AddComponent("gcwu_psnb", $gcwu_psnb);
+$TopMenu->AddComponent("gcwu_gcnb", $gcwu_gcnb);
+$Content->AddComponent("main", $main);
 $ScriptIncludes = "";
 $SList = explode("|", $Scripts);
 foreach ($SList as $Script) {
@@ -159,53 +154,49 @@ $Attributes->SetValue("pathToRoot", "");
 $Attributes->Show();
 //End Initialize HTML Template
 
-//Execute Components @1-DF377014
+//Execute Components @1-D50AF6A0
 $MasterPage->Operations();
-$gcwu_breadcrum->Operations();
-$gcwu_psnb->Operations();
-$main_basic->Operations();
-$gcwu_footer->Operations();
+$main->Operations();
 $gcwu_gcnb->Operations();
+$gcwu_psnb->Operations();
+$gcwu_breadcrum->Operations();
+$gcwu_footer->Operations();
 //End Execute Components
 
-//Go to destination page @1-C0856F3F
+//Go to destination page @1-C0156DEF
 if($Redirect)
 {
     $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
     header("Location: " . $Redirect);
-    $gcwu_gcnb->Class_Terminate();
-    unset($gcwu_gcnb);
     $gcwu_footer->Class_Terminate();
     unset($gcwu_footer);
-    $main_basic->Class_Terminate();
-    unset($main_basic);
-    $gcwu_psnb->Class_Terminate();
-    unset($gcwu_psnb);
     $gcwu_breadcrum->Class_Terminate();
     unset($gcwu_breadcrum);
+    $gcwu_psnb->Class_Terminate();
+    unset($gcwu_psnb);
+    $gcwu_gcnb->Class_Terminate();
+    unset($gcwu_gcnb);
+    $main->Class_Terminate();
+    unset($main);
     unset($Tpl);
     exit;
 }
 //End Go to destination page
 
-//Show Page @1-19821289
+//Show Page @1-A663C207
 $Head->Show();
-$TopMenu->Show();
-$CustomCSS->Show();
-$CustomScripts->Show();
 $Footer->Show();
-$Content->Show();
-$SiteMenu->Show();
+$CustomScripts->Show();
+$CustomCSS->Show();
 $Breadcrum->Show();
-$NavMenu->Show();
+$SiteMenu->Show();
+$TopMenu->Show();
+$Content->Show();
 $MasterPage->Tpl->SetVar("Head", $Tpl->GetVar("Panel Head"));
-$MasterPage->Tpl->SetVar("TopMenu", $Tpl->GetVar("Panel TopMenu"));
-$MasterPage->Tpl->SetVar("CustomCSS", $Tpl->GetVar("Panel CustomCSS"));
-$MasterPage->Tpl->SetVar("CustomScripts", $Tpl->GetVar("Panel CustomScripts"));
 $MasterPage->Tpl->SetVar("Footer", $Tpl->GetVar("Panel Footer"));
-$MasterPage->Tpl->SetVar("Content", $Tpl->GetVar("Panel Content"));
-$MasterPage->Tpl->SetVar("SiteMenu", $Tpl->GetVar("Panel SiteMenu"));
 $MasterPage->Tpl->SetVar("cgwu_breadcrum", $Tpl->GetVar("Panel Breadcrum"));
+$MasterPage->Tpl->SetVar("SiteMenu", $Tpl->GetVar("Panel SiteMenu"));
+$MasterPage->Tpl->SetVar("TopMenu", $Tpl->GetVar("Panel TopMenu"));
 $MasterPage->Show();
 if (!isset($main_block)) $main_block = $MasterPage->HTML;
 $main_block = CCConvertEncoding($main_block, $FileEncoding, $CCSLocales->GetFormatInfo("Encoding"));
@@ -213,19 +204,19 @@ $CCSEventResult = CCGetEvent($CCSEvents, "BeforeOutput", $MainPage);
 if ($CCSEventResult) echo $main_block;
 //End Show Page
 
-//Unload Page @1-A308E7BD
+//Unload Page @1-2802F93B
 $CCSEventResult = CCGetEvent($CCSEvents, "BeforeUnload", $MainPage);
 unset($MasterPage);
-$gcwu_gcnb->Class_Terminate();
-unset($gcwu_gcnb);
 $gcwu_footer->Class_Terminate();
 unset($gcwu_footer);
-$main_basic->Class_Terminate();
-unset($main_basic);
-$gcwu_psnb->Class_Terminate();
-unset($gcwu_psnb);
 $gcwu_breadcrum->Class_Terminate();
 unset($gcwu_breadcrum);
+$gcwu_psnb->Class_Terminate();
+unset($gcwu_psnb);
+$gcwu_gcnb->Class_Terminate();
+unset($gcwu_gcnb);
+$main->Class_Terminate();
+unset($main);
 unset($Tpl);
 //End Unload Page
 
