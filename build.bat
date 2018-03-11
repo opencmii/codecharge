92f2b90ab3cd75a4d43574e55fb415940d712663
sysop <sysop@apexsciences.com>
2018-03-11 17:31:51 -0400

    Quick build script
    Copy demo-ccs to local ssumed XAMPP based APache MySQL PHP distribution. Not to be used for production server!

--- build.bat
+++ build.bat
@@ -1,6 +1,34 @@
-rem
-SET TEST=c:\xampp\htdocs\codecharge
-mkdir %TEST%
-xcopy /S /D .\* %TEST%
-pause
+REM =================================
+REM DEV build and install process 
+REM =================================
+REM purpose: install demo-ccs application on localhost for TEST only.
+REM production installation requires ususally more considerations (file priviledges, MySQL user management,...)
+REM ---------------------------------
+REM Overview
+REM ---------------------------------
+REM 1. setup your LAMP server
+REM Create a LAMP server. Suggestion: Install bitnami XAMPP Distribution locally.
+REM create a demo user, and demo database using provided demo_MySQL.sql file in /data folder
+REM hint: if using XAMPP, use phpmyadmin to copy-and-paste the content of demo_MySQL.sql into phpmyadmin sql TAB;
+
+REM 2. setup demo application
+REM Copy directory demo-ccs to your LAMP server; 
+REM update Common.php with demo user and database info; 
+REM make Common.php readable only by server... 
+REM ==================================
+REM INSTALL
+REM ==================================
+REM Assumptions: 
+REM 1. local Bitnami XAMPP Distribution installed by default (in c:\xampp) (Windows)
+SET WWW=c:\xampp\htdocs\demo-ccs
+REM -----------------------------------
+REM Procedure
+REM -----------------------------------
+REM Backup present installed application just in case...
+REM DELETE %WWW%_old
+REM RENAME %WWW% %WWW%_old 
+
+REM (Re-)install demo-ccs from local directory where this file reside.
+REM MKDIR %WWW%
+XCOPY /S /D .\* %WWW%
 
